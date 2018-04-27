89d5c4a30e15147a14400c558a2b9fe447e6bc9a
Austin Winarski <ajwinarski@gmail.com>
2018-04-26 22:49:25 -0400

    Add files via upload

--- AudioListener.py
+++ AudioListener.py
@@ -24,29 +24,29 @@ class Recorder():
 
         #Global Variables
         self.parser = argparse.ArgumentParser(description=__doc__)
-        parser.add_argument('-l', '--list-devices', action='store_true',
+        self.parser.add_argument('-l', '--list-devices', action='store_true',
                             help='show list of audio devices and exit')
-        parser.add_argument('-d', '--device', type=int_or_str,
+        self.parser.add_argument('-d', '--device', type=int_or_str,
                             help='input device (numeric ID or substring)')
-        parser.add_argument('-r', '--samplerate', type=int, help='sampling rate')
-        parser.add_argument('-c', '--channels', type=int, default=1, help='number of input channels')
-        parser.add_argument('filename', nargs='?', metavar='FILENAME',
+        self.parser.add_argument('-r', '--samplerate', type=int, help='sampling rate')
+        self.parser.add_argument('-c', '--channels', type=int, default=1, help='number of input channels')
+        self.parser.add_argument('filename', nargs='?', metavar='FILENAME',
                             help='audio file to store recording to')
-        parser.add_argument('-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
-        self.args = parser.parse_args()
+        self.parser.add_argument('-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
+        self.args = self.parser.parse_args()
         args.filename = tempfile.mkstemp(prefix=time.strftime("%d%m%Y-%S%M%H"),
                                         suffix='.wav', dir='')
 
         self.state = GPIO.input(17)     # BUTTON = 17
         self.recording = False
 
-        if args.list_devices:
+        if self.args.list_devices:
             print(sd.query_devices())
-            parser.exit(0)
-        if args.samplerate is None:
-            device_info = sd.query_devices(args.device, 'input')
+            self.parser.exit(0)
+        if self.args.samplerate is None:
+            device_info = sd.query_devices(self.args.device, 'input')
             # soundfile expects an int, sounddevice provides a float:
-            args.samplerate = int(device_info['default_samplerate'])
+            self.args.samplerate = int(device_info['default_samplerate'])
 
 
     def record(self):
